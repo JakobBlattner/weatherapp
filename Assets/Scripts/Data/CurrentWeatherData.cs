0379a811988332784e75e2a67e1a0f6ff9513e98
JakobBlattner <jakoblattner@gmail.com>
2021-05-26 10:53:25 +0200

    initial commit, work from last 2 weeks

---
+++ Assets/Scripts/Data/CurrentWeatherData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Data
+{
+    public class CurrentWeatherData : WeatherData
+    {
+        public float temp { get; set; }
+        public float feels_like { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+        public Rain rain { get; set; }
+        public Snow snow{ get; set; }
+    }
+}
